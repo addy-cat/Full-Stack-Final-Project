ff12622863c0439b5378560538838393101afde7
addy_cat <aswantz@pdx.edu>
2022-03-14 12:09:43 -0700

    Updated README

--- src/components/YouWin.js
+++ src/components/YouWin.js
@@ -3,7 +3,7 @@ import React from 'react'
 class YouWin extends React.Component {
     render() {
         return (
-            <div style={{fontSize: "50%", position: "absolute", top: "35%", left: "13%", zIndex: "5", padding:"5% 7%"}} className="alert alert-success" role="alert">
+            <div style={{fontSize: "50%", position: "absolute", top: "35%", left: "20%", zIndex: "5", padding:"5% 7%"}} className="alert alert-success" role="alert">
                 <h4 style={{fontSize: "3rem"}} className="alert-heading">You Win!</h4>
             </div>
             
